89b540aa66ecc6047ca920f9a3e10b5294b23ee2
Phong Nguyen <p.nguyen@city.ac.uk>
2018-05-15 16:21:01 +0100

    draft of first visualisation

---
+++ demo/thread/index.js
@@ -0,0 +1,82 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Instantiate vis and its parameters
+    const vis = pv.vis.thread();
+
+    // Make the vis responsive to window resize
+    window.onresize = _.throttle(update, 100);
+
+    // Data
+    let allSessions,
+        allTopics,
+        threadData;
+
+    d3.json('../../data/long-threads.json').then(data => {
+        threadData = data[0].messages;
+        threadData.forEach(m => {
+            m.time = new Date(m.time);
+        });
+        console.log(threadData);
+
+        // allSessions = data.sessions;
+        // allTopics = data.topics.map((t, i) => ({
+        //     id: i,
+        //     // label: '(' + t.map(a => a.action).join(', ') + ')'
+        //     label: 'Task ' + (i + 1)
+        // }));
+
+        // buildUserDropdown();
+
+        // Display the first user
+        // const userId = document.querySelector('#userSelect').options[0].value;
+        // userData = buildUserData(userId);
+
+        // Build the vis
+        update();
+    });
+
+    /**
+     * Updates vis when window changed.
+     */
+    function update() {
+        // Update size of the vis
+        vis.width(window.innerWidth)
+            .height(window.innerHeight);
+
+        // Update size of the vis container and redraw
+        d3.select('.pv-vis-demo')
+            .attr('width', window.innerWidth)
+            .attr('height', window.innerHeight)
+            .datum(threadData)
+            .call(vis);
+    }
+
+    function buildUserData(userId) {
+        const columns = allSessions
+            .filter(s => s.userId === userId)
+            .sort((a, b) => d3.ascending(new Date(a.time), new Date(b.time)))
+            .map((s, i) => ({
+                id: s.id,
+                label: 'S' + (i + 1),
+                segments: s.segments.map(seg => seg.topics.map(t => ({ rowId: t.topicId, value: t.prob })))
+            }));
+
+        return {
+            columns: columns,
+            rows: allTopics
+        }
+    }
+
+    function buildUserDropdown() {
+        const select = document.querySelector('#userSelect');
+        _.each(_.groupBy(allSessions, d => d.userId), (v, k) => {
+            select.options.add(new Option(k + ' (' + v.length + ')', k));
+        });
+
+        // Display sessions of the selected user
+        select.addEventListener('change', function() {
+            userData = buildUserData(this.value);
+            vis.invalidate();
+            update();
+        });
+    }
+});
---
+++ src/js/thread.js
@@ -0,0 +1,255 @@
+/**
+ * A visualization of an email thread.
+ * Data input:
+ * - array of messages, each has:
+ *  - messageId, subject, sender, time
+ *  - array of recipients, each has email and type
+ */
+pv.vis.thread = function() {
+    /**
+     * Visual configs.
+     */
+    const margin = { top: 5, right: 10, bottom: 5, left: 5 },
+        radius = 5,
+        personHeight = 16,
+        personLabelWidth = 90; // this value is also defined in css
+
+    let visWidth = 960, visHeight = 600, // Size of the visualization, including margins
+        width, height; // Size of the main content, excluding margins
+
+    /**
+     * Accessors.
+     */
+    let messageId = d => d.messageId,
+        subject = d => d.subject,
+        sender = d => d.sender,
+        time = d => d.timestamp,
+        recipients = d => d.recipients,
+        email = d => d.email,
+        type = d => d.type;
+
+    /**
+     * Data binding to DOM elements.
+     */
+    let data,
+        personData,
+        personLookup,
+        dataChanged = true; // True to redo all data-related computations
+
+    /**
+     * DOM.
+     */
+    let visContainer, // Containing the entire visualization
+        personContainer;
+
+    /**
+     * D3.
+     */
+    const listeners = d3.dispatch('click'),
+        xSeqScale = d3.scaleLinear(),
+        xContScale = d3.scaleLinear();
+
+    /**
+     * Main entry of the module.
+     */
+    function module(selection) {
+        selection.each(function(_data) {
+            // Initialize
+            if (!this.visInitialized) {
+                visContainer = d3.select(this).append('g').attr('class', 'pv-thread');
+                personContainer = visContainer.append('g').attr('class', 'persons');
+
+                this.visInitialized = true;
+            }
+
+            data = _data.sort((a, b) => d3.ascending(time(a), time(b)));
+            update();
+        });
+
+        dataChanged = false;
+    }
+
+    /**
+     * Updates the visualization when data or display attributes changes.
+     */
+    function update() {
+        // Canvas update
+        width = visWidth - margin.left - margin.right;
+        height = visHeight - margin.top - margin.bottom;
+
+        visContainer.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        xSeqScale.range([personLabelWidth, width]);
+        xContScale.range([personLabelWidth, width]);
+
+        /**
+         * Computation.
+         */
+        // Updates that depend only on data change
+        if (dataChanged) {
+            personData = buildPersonData(data);
+            addMessageInstances(data);
+
+            xSeqScale.domain([0, data.length - 1]);
+        }
+
+        // Updates that depend on both data and display change
+        layoutPersons(personData);
+        layoutMessages(data);
+
+        /**
+         * Draw.
+         */
+        const persons = personContainer.selectAll('.person').data(personData, d => d.email);
+        persons.enter().append('g').attr('class', 'person').call(enterPersons)
+            .merge(persons).call(updatePersons);
+        persons.exit().transition().attr('opacity', 0).remove();
+    }
+
+    function buildPersonData(messages) {
+        // Persons include both senders and recipients.
+        // Senders are added first, sorted by time.
+        // Then recipients who don't send are added, sort by time as well.
+        // Messages are expected to be already sorted by time.
+        const persons = [];
+        personLookup = {};
+
+        function addperson(s) {
+            if (!personLookup[s]) {
+                persons.push({ email: s, instances: [] });
+                personLookup[s] = _.last(persons);
+            }
+        }
+
+        messages.map(sender).forEach(addperson);
+
+        messages.map(recipients).forEach(r => {
+            r.map(email).forEach(addperson);
+        });
+
+        return persons;
+    }
+
+    function addMessageInstances(messages) {
+        messages.forEach((m, i) => {
+            personLookup[sender(m)].instances.push(createNewMessageInstance(i, true));
+
+            recipients(m).forEach(r => {
+                personLookup[email(r)].instances.push(createNewMessageInstance(i, false, type(r)));
+            });
+        });
+    }
+
+    function createNewMessageInstance(idx, isSender, type) {
+        return {
+            messageIndex: idx,
+            isSender: isSender,
+            type: type
+        };
+    }
+
+    function enterPersons(selection) {
+        const container = selection
+            .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')')
+            .attr('opacity', 0);
+
+        const fo = container.append('foreignObject').attr('width', '100%').attr('height', '100%');
+
+        fo.append('xhtml:div').attr('class', 'label')
+            .text(d => d.email.substr(0, d.email.indexOf('@')));
+        fo.append('title')
+            .text(d => d.email);
+    }
+
+    function updatePersons(selection) {
+        selection.each(function(d) {
+            const container = d3.select(this);
+
+            container.transition()
+                .attr('transform', 'translate(' + d.x + ',' + d.y + ')')
+                .attr('opacity', 1);
+
+            layoutInstances(d.instances);
+            const instances = container.selectAll('.instance').data(d.instances, d => d.messageIndex);
+            instances.enter().append('g').attr('class', 'instance').call(enterInstances)
+                .merge(instances).call(updateInstances);
+            instances.exit().transition().attr('opacity', 0).remove();
+        });
+    }
+
+    function enterInstances(selection) {
+        const container = selection
+            .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')')
+            .attr('opacity', 0);
+
+        container.append('circle')
+            .attr('r', radius);
+    }
+
+    function updateInstances(selection) {
+        selection.each(function(d) {
+            const container = d3.select(this);
+
+            container.transition()
+                .attr('transform', 'translate(' + d.x + ',' + d.y + ')')
+                .attr('opacity', 1);
+
+            container.classed('sender', d.isSender);
+        });
+    }
+
+    function layoutPersons(persons) {
+        persons.forEach((p, i) => {
+            p.x = 0;
+            p.y = personHeight * i;
+        });
+    }
+
+    function layoutMessages(messages) {
+        messages.forEach((m, i) => {
+            m.x = xSeqScale(i);
+            m.y = 0;
+        });
+    }
+
+    function layoutInstances(instances) {
+        instances.forEach(i => {
+            i.x = data[i.messageIndex].x;
+            i.y = (personHeight - radius) / 2 + 1;
+        });
+    }
+
+    /**
+     * Sets/gets the width of the visualization.
+     */
+    module.width = function(value) {
+        if (!arguments.length) return visWidth;
+        visWidth = value;
+        return this;
+    };
+
+    /**
+     * Sets/gets the height of the visualization.
+     */
+    module.height = function(value) {
+        if (!arguments.length) return visHeight;
+        visHeight = value;
+        return this;
+    };
+
+    /**
+     * Sets the flag indicating data input has been changed.
+     */
+    module.invalidate = function() {
+        dataChanged = true;
+    };
+
+    /**
+     * Binds custom events.
+     */
+    module.on = function() {
+        const value = listeners.on.apply(listeners, arguments);
+        return value === listeners ? module : value;
+    };
+
+    return module;
+};
